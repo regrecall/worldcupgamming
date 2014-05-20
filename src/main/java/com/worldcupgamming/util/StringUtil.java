24463199d38a18cda244ae0b3b4c5d4c68c90339
regrecall <regrecall@gmail.com>
2014-05-20 17:39:17 +0800

    init project

---
+++ src/main/java/com/worldcupgamming/util/StringUtil.java
@@ -0,0 +1,15 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2014 All Rights Reserved.
+ */
+package com.worldcupgamming.util;
+
+/**
+ * @author xingchi.wxc
+ * @version $Id: StringUtil, v 0.1 14-5-20 обнГ5:12 xingchi.wxc Exp $$
+ */
+public final class StringUtil {
+    public static String test() {
+        return "StringUtil + xingchi.wxc";
+    }
+}
